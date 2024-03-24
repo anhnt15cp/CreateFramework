ddbed592e28edf946b31ed1c867f5f6c70389dbc
Tuấn Anh <anhnt682@viettel.com.vn>
2024-03-24 19:38:12 +0700

    - init

---
+++ CreateFramework/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CreateFramework
+//
+//  Created by Tuấn Anh on 24/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let a: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
